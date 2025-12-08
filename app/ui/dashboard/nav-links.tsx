8cbbcf762081a653a98cfe3630e7d0612507489d
Zunda-zunda <yuuya.nouzaki@docomo.ne.jp>
2025-12-08 16:10:20 +0900

    7の途中だけどレイアウトが未完成

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -9,8 +9,6 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
@@ -23,12 +21,13 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <link
+          <Link
             key={link.name}
             href={link.href}
             className={clsx(
@@ -40,7 +39,7 @@ export default function NavLinks() {
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </link>
+          </Link>
         );
       })}
     </>
